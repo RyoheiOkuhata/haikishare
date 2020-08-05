1ab94824d4a1d6669ba521b4a604ef325f06ddf7
Ryohei Okuhata <>
2020-08-05 17:23:34 +0900

    コミットメッセージ

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -6,7 +6,7 @@
       <a href="{{route('products.show', ['product_id' => $product])}}">
          <div class="p-card--gallery__img">
            <p class="p-card--gallery__img">
-            <img alt="文字表示" src="  https://haiki.s3-ap-northeast-1.amazonaws.com/products_images/'.{{$product->img}}.jpg">
+            <img alt="文字表示" src="https://haiki.s3-ap-northeast-1.amazonaws.com/public/products_images/.{{$product->img}}.jpg">
 
            </p>
           </div>
