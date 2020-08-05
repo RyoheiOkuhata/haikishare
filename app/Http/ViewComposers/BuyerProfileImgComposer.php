6032386d7b70ccdd077fe855f864207a6fb6a115
Ryohei Okuhata <>
2020-08-05 21:57:58 +0900

    コミットメッセージ

--- app/Http/ViewComposers/BuyerProfileImgComposer.php
+++ app/Http/ViewComposers/BuyerProfileImgComposer.php
@@ -14,15 +14,14 @@ class BuyerProfileImgComposer {
   public function __construct(){
 
 }
-
   public function compose(View $view) {
     $buyer = Auth::guard('buyers')->user();
     $is_img = false;
-
   if($buyer){
-  if(Auth::guard('buyers')->user() && Storage::disk('s3')->get('public/buyerProfile_images/'.$buyer->id. '.jpg')){
+  if(Auth::guard('buyers')->user() && Storage::disk('s3')->exists('public/buyerProfile_images/'.$buyer->id. '.jpg')){
     $is_img = true;
   }
+  Log::debug(print_r($is_img, true));
     $view->with('is_img',$is_img);
     }
   }
