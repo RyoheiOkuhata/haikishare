17e7a55f6b4e7a4075ec381e1ba0956ca4392b70
Ryohei Okuhata <>
2020-08-06 13:32:49 +0900

    A

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -74,27 +74,18 @@ class OrderController extends Controller
  public function done(Request $request,$id) {
 
    $product = Product::find($id);
-   $user = $product->user;
-   $buyer = Auth::guard('buyers')->user();
+
 
         $product->orderBuyer()->detach( Auth::guard('buyers')->user()->id);
         //二重登録防止のため、一度消去
         $product->orderBuyer()->attach( Auth::guard('buyers')->user()->id);
         //$product->orders()でproductモデルからordersテーブル経由で紐付いているバイヤーモデルのコレクションが返
 
-        $shop =$user;
-        $buyer_name =$buyer;
-        $to = [
-          'test_01@mail.com',
-          'test_02@mail.com'
-      ];
-
-        Mail::to($to)->send(new PurchaseNotification($shop, $product,$buyer_name));
 
 
   return redirect()->route('products.index')->with('flash_message', '購入が完了されました');;
     }
 }
 
-51_1596643262.jpg
+
 
