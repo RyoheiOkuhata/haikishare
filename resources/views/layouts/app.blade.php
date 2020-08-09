6d24b1b50ed5f132c25b89d5edc35a0dce5a7e24
Ryohei Okuhata <>
2020-08-09 17:19:28 +0900

    ファイル名変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,13 +22,6 @@
       </p>
 </div>
 
-@if (session('status'))
-    <div class="flash_message">
-      <p class="flash_meg--txt">  {{ session('status') }}
-      <i class="far fa-thumbs-up"></i>
-      </p>
-</div>
-@endif
 
 @endif
 <div id="app">
